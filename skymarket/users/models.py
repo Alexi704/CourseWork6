b171c268567d0fed6b2c159b8f75197ceb813315
Tuman8 <i-a-l-e-x@yandex.ru>
2022-08-17 23:14:21 +0700

    модель User + serializers

--- skymarket/users/models.py
+++ skymarket/users/models.py
@@ -1,16 +1,24 @@
 from django.contrib.auth.models import AbstractBaseUser, AbstractUser
 from django.db import models
-from managers import UserManager
+# from .managers import UserManager
 from phonenumber_field.modelfields import PhoneNumberField
 from django.utils.translation import gettext_lazy as _
 
 
 class UserRoles:
-    # TODO закончите enum-класс для пользователя
-    pass
+    ADMIN = 'admin'
+    USER = 'user'
+    ROLES = [(ADMIN, 'Админ'), (USER, 'Пользователь')]
 
 
 class User(AbstractBaseUser):
-    # TODO переопределение пользователя.
-    # TODO подробности также можно поискать в рекоммендациях к проекту
-    pass
+    first_name = models.CharField(max_length=40)
+    last_name = models.CharField(max_length=40)
+    phone = PhoneNumberField
+    email = models.EmailField(unique=True)
+    role = models.CharField(max_length=12, choices=UserRoles.ROLES, default='user')
+    images = models.ImageField(null=True, blank=True)
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
