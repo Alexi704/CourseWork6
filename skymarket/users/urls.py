8485ae84381a0ba1e915c2684bd8ca322a4c1115
Tuman8 <i-a-l-e-x@yandex.ru>
2022-08-24 20:35:15 +0300

    urls пользователя

--- skymarket/users/urls.py
+++ skymarket/users/urls.py
@@ -1,13 +1,10 @@
 from django.urls import include, path
 from djoser.views import UserViewSet
 from rest_framework.routers import SimpleRouter
-# TODO подключите UserViewSet из Djoser.views к нашим urls.py
-# TODO для этого рекоммендуется использовать SimpleRouter
-
-urlpatterns = [
-]
 
 users_router = SimpleRouter()
-users_router.register("user", UserViewSet)
+users_router.register("users", UserViewSet, basename='users')
 
-urlpatterns += users_router.urls
+urlpatterns = [
+    path("", include(users_router.urls))
+]
