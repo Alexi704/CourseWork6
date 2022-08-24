0d510c3af057bfd550a7a34d179b318e6959a3eb
Tuman8 <i-a-l-e-x@yandex.ru>
2022-08-24 20:25:38 +0300

    managers.py

--- skymarket/users/managers.py
+++ skymarket/users/managers.py
@@ -1,7 +1,35 @@
 from django.contrib.auth.models import (
     BaseUserManager
 )
-# TODO здесь должен быть менеджер для модели Юзера.
-# TODO Поищите эту информацию в рекомендациях к проекту
+
+
+# менеджер для модели Юзера.
 class UserManager(BaseUserManager):
-    pass
+    def create_user(self, email, first_name, last_name, phone, password=None):
+        """Создание и запись пользователя с использованием email и пароля"""
+        if not email:
+            raise ValueError('Users nust have an email address')
+        user = self.model(
+            email=self.normalize_email(email),
+            first_name=first_name,
+            last_name=last_name,
+            phone=phone,
+        )
+        user.role = 'user',
+        user.is_active = True,
+        user.set_password(password),
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, first_name, last_name, phone, password=None):
+        """Создание и запись суперпользователя"""
+        user = self.model(
+            email=self.normalize_email(email),
+            first_name=first_name,
+            last_name=last_name,
+            phone=phone,
+        )
+        user.role = 'admin',
+        user.save(using=self._db)
+        return user
